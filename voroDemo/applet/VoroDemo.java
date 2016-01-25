5ae8d2dec01fe0cbd8484714e637ffedbfc91a4b
vvclin-git <vvclin@gmail.com>
2016-01-25 08:51:27 +0800

    runnable jar added

--- voroDemo/applet/VoroDemo.java
+++ voroDemo/applet/VoroDemo.java
@@ -1,6 +1,7 @@
 package vorodemo;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.TreeMap;
 
@@ -8,75 +9,24 @@ import processing.core.PApplet;
 
 
 public class VoroDemo extends PApplet {
-	//PApplet canvas;
-	ArrayList<Parabola> parabolae = new ArrayList<Parabola>();
-	//ArrayList<Arc> arcs = new ArrayList<Arc>();
-	ArrayList<BreakPoint> breakPoints = new ArrayList<BreakPoint>();
-	ArrayList<Site> sitesAbove = new ArrayList<Site>();
-	ArrayList<Site> sitesBelow = new ArrayList<Site>();
-	ArrayList<Edge> edges = new ArrayList<Edge>();
-	ArrayList<Circle> circles = new ArrayList<Circle>();
-//	TreeMap<Point, Point> beachLine = new TreeMap<Point, Point>();
-	TreeMap<BptNode, BptNode> beachLine = new TreeMap<BptNode, BptNode>();
-	TreeMap<BptNode, Parabola> arcs = new TreeMap<BptNode, Parabola>();
-	//TreeMap<Float, Site> sitesTree = new TreeMap<Float, Site>();
-	Directrix dictx = new Directrix(100, this);	
+	PApplet canvas;
+	Voronoi voronoi = new Voronoi(100, this);
+	Integer step = 1;
 	
-	private void siteEvent(Site site) {
-		if (beachLine.isEmpty()) {
-			Parabola newPara = new Parabola(dictx, site, 600, this);
-			BptNode leftNode = new BptNode("leftBound", null, site, dictx);
-			BptNode rightNode = new BptNode("rightBound", site, null, dictx);
-			beachLine.put(leftNode, leftNode);
-			beachLine.put(rightNode, rightNode);
-			// for drawing beach line
-			breakPoints.add(newPara.leftBpt);
-			breakPoints.add(newPara.rightBpt);
-			arcs.put(leftNode, newPara);
-		}
-		else {
-			// determine which arc to insert a site
-			BptNode queryNode = new BptNode("query", site);
-			BptNode oldLeftNode = beachLine.floorEntry(queryNode).getValue();
-			BptNode oldRightNode = beachLine.ceilingEntry(queryNode).getValue();
-			// for drawing beach line
-			Parabola oldPara = arcs.get(oldLeftNode);
-			BreakPoint oldLeftBpt = oldPara.leftBpt;
-			BreakPoint oldRightBpt = oldPara.rightBpt;
-			Parabola newPara = new Parabola(dictx, site, oldPara, this);
-			// create new break points
-			BptNode newLeftNode = new BptNode("left", oldLeftNode.rightSite, site, dictx);
-			BptNode newRightNode = new BptNode("right", site, oldRightNode.leftSite, dictx);
-			beachLine.put(newLeftNode, newLeftNode);
-			beachLine.put(newRightNode, newRightNode);
-			// for drawing beach line
-			BreakPoint newLeftBpt = newPara.leftBpt;
-			BreakPoint newRightBpt = newPara.rightBpt;			
-			breakPoints.add(newLeftBpt);
-			breakPoints.add(newRightBpt);
-			// add new arcs (for drawing beach line)
-			arcs.remove(oldLeftNode);
-			arcs.put(oldLeftNode, new Parabola (dictx, oldLeftNode.rightSite, oldLeftBpt, newLeftBpt, this));
-			arcs.put(newLeftNode, newPara);
-			arcs.put(newRightNode, new Parabola (dictx, oldLeftNode.rightSite, newRightBpt, oldRightBpt, this));			
-		}
-	}
-	private void updateBpts() {
-		for (BreakPoint bpt : breakPoints) {
-			bpt.update();
-		}
-	}
-	public void mouseClicked() {  
-		Site site = new Site(mouseX, mouseY, this);		
-		if (site.y() > dictx.y()) {
-			System.out.print(site + ", ");
-			sitesBelow.add(site);
-			//Collections.sort(sitesBelow);
-		}
-		else if (site.y() < dictx.y()) {
-			sitesAbove.add(site);
-			//Collections.sort(sitesAbove);
-		} 
+	public void mouseClicked() {
+		//Event[] eventsOut;
+		if (mouseY > voronoi.dictx.y()) {
+			System.out.print("(" + mouseX + ", " + mouseY + "), ");			
+			voronoi.addSite(new Site(mouseX, mouseY, this));
+			//for debugging
+//			Event[] eventsOut = new Event[voronoi.events.size()];
+//			eventsOut = voronoi.events.toArray(new Event[voronoi.events.size()]);
+//			System.out.println();
+//			Arrays.sort(eventsOut);
+//			for (Event event : eventsOut) {
+//				System.out.print(event.toString() + ",");
+//			}
+		}		
 	}
 	public void keyPressed() {
 		if (key == CODED) {
@@ -86,60 +36,37 @@ public class VoroDemo extends PApplet {
 //			System.out.print(" below " + sitesBelow.size());
 //			if (!arcs.isEmpty()) {
 //				System.out.println(arcs.get(0).leftX + " " + arcs.get(0).rightX);
-//			}
-			Site siteTmpRef = null;
+//			}			
 			if (keyCode == UP) { // not necessary
-//				dictx.move(-1);				
-//				for (Site site : sitesAbove) {
-//					if (site.y() > dictx.y()) {
-//						sitesBelow.add(site.copy());
-//						siteTmp = site;						
-//					}
-//				}
-//				sitesAbove.remove(siteTmp);
 			} 
-			else if (keyCode == DOWN) {
-				dictx.move(1);				
-				updateBpts();				
-				//updateArc();
-				for (Site site : sitesBelow) {
-					if (site.y() < dictx.y()) {
-						sitesAbove.add(site.copy());
-						siteTmpRef = site;
-						//System.out.println(sitesAbove.size());
-						if (sitesAbove.size() >= 3) {
-							for (int i = 0; i <= sitesAbove.size() - 3; i++) {
-								circles.add(new Circle(sitesAbove.get(i), sitesAbove.get(i + 1), sitesAbove.get(i + 2), this));
-							}
-							Collections.sort(circles);
+			else if (keyCode == DOWN) {				
+//				voronoi.dictx.move(1);
+//				voronoi.update();
+				//System.out.println(voronoi.circles.size());
+				// event
+				if (!voronoi.events.isEmpty()) {					
+					if (voronoi.events.peek().y() - voronoi.dictx.y() <= step) {
+						//voronoi.dictx.setY(Math.round((voronoi.events.peek().y() / step)) * step);
+						voronoi.dictx.setY(voronoi.events.peek().y());						
+						while (voronoi.events.peek().y() == voronoi.dictx.y()) { // "<=" is a must have in the future						
+							voronoi.events.poll().eventHandler();
+							// for debugging
+							//System.out.println(voronoi.arcs.size());
+							if (voronoi.events.isEmpty()) break;
+							voronoi.update();
 						}
-						//for debugging
-//						for (Site site1 : sitesBelow) {
-//							System.out.print(site1.y() + ", ");
-//						}
-//						System.out.println();
-//						System.out.println("test");
+//						voronoi.dictx.setY(voronoi.events.peek().y());
+						voronoi.update();
+					}
+					else {
+						voronoi.dictx.move(step);
+						voronoi.update();
 					}
 				}
-				if (siteTmpRef != null) {
-					siteEvent(siteTmpRef.copy());
-					System.out.println();
-					for (Parabola arc : arcs.values()) {						
-						System.out.print(arc.leftBpt.type + ", " + arc.rightBpt.type + "|");
-					}
-					System.out.println();
-					for (Parabola arc : arcs.values()) {
-						System.out.print("(" + arc.leftBpt.x() + ", "  + arc.leftBpt.y() + "), " + "(" + arc.rightBpt.x() + ", " +arc.rightBpt.y() + ")" + "|");						
-					}
-					System.out.println();
-//					for (Parabola arc : arcs.values()) {
-//						System.out.print(arc.leftBpt.y() + ", " + arc.rightBpt.y() + "|");
-//					}
-//					System.out.println();
-					sitesBelow.remove(siteTmpRef);					
-					siteTmpRef = null;
+				else {
+					voronoi.dictx.move(step);
+					voronoi.update();
 				}
-				
 			} 
 		} 
 	}
@@ -149,48 +76,47 @@ public class VoroDemo extends PApplet {
 	}
 	public void settings() {		
 		size(600, 600);
-		//float[][] testSites = {{376.0f, 122.0f},{269.0f, 126.0f},{151.0f, 162.0f},{98.0f, 212.0f}};
-//		float[][] testSites = {{376.0f, 122.0f},{269.0f, 122.0f},{151.0f, 122.0f},{98.0f, 122.0f}}; 
-//		for (float[] coord : testSites) {
-//			sitesBelow.add(new Site(coord[0], coord[1], this));
-//		}
-	}
-	
-	public void drawBeachLine() {
-		for (Parabola arc : arcs.values()) {
-			arc.draw();
-		}
+		// known good sites
+//		voronoi.addSite(new Site(270, 118, this));
+//		voronoi.addSite(new Site(252, 126, this));
+//		voronoi.addSite(new Site(287, 136, this));
+		// known good
+		voronoi.addSite(new Site(320, 124, this));
+		voronoi.addSite(new Site(269, 130, this));		
+		voronoi.addSite(new Site(343, 157, this));
+		voronoi.addSite(new Site(352, 217, this));
+		voronoi.addSite(new Site(472, 585, this));
+		voronoi.addSite(new Site(123, 460, this));
+		voronoi.addSite(new Site(413, 346, this));
+		voronoi.addSite(new Site(180, 285, this));
+		
+		// known good
+//		voronoi.addSite(new Site(320, 124, this));
+//		voronoi.addSite(new Site(269, 130, this));		
+//		voronoi.addSite(new Site(343, 157, this));
+//		voronoi.addSite(new Site(200, 217, this));
+		
+//		// known bad sites (work sometimes?)
+//		voronoi.addSite(new Site(320, 124, this));
+//		voronoi.addSite(new Site(269, 130, this));		
+//		voronoi.addSite(new Site(343, 157, this));
+		
+		//
+//		voronoi.addSite(new Site(320, 124, this));
+//		voronoi.addSite(new Site(269, 130, this));		
+//		voronoi.addSite(new Site(343, 130, this));
+		//voronoi.addSite(new Site(200, 217, this));
+		
+		
 
-	}
+	}	
 
 	public void draw() {
 		clear();
-		dictx.draw();		
-		if (!sitesAbove.isEmpty()) {
-			for (Site site : sitesAbove) {
-				site.draw();
-			}
-		}
-		if (!sitesBelow.isEmpty()) {
-			for (Site site : sitesBelow) {
-				site.draw();
-			}
-		}
-		for (BreakPoint bpt : breakPoints) {
-			bpt.draw();
-		}
-		drawBeachLine();
-		
-//		if (!circles.isEmpty()) {
-//			for (Circle circle : circles) {
-//				circle.draw();
-//			}
-//		}
-
-		
+		voronoi.draw();		
 	}
-	public static void main(String args[]) {
-		
+	public static void main(String args[]) {		
 	    PApplet.main(new String[] { vorodemo.VoroDemo.class.getName() });
+	    
 	}
 }
